523bc2efb704ef8c9ddbecba352cd7fe8aa57c7d
Dawid Oberda <dawidoberda@gmail.com>
2020-02-28 09:24:58 +0100

    back to good version

--- histogram.py
+++ histogram.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from configparser import ConfigParser
+from datetime import date
 
 parser = ConfigParser()
 parser.read('conf.ini')
@@ -15,18 +16,43 @@ excel_path = os.path.join('./input_data', 'raport.xlsx')
 print((os.path.exists(excel_path)))
 
 df = pd.read_excel(excel_path)
-#df['AssemblyName'] = df['AssemblyName'].astype(str)
 
-#print(df.head(10))
 #is_selected_ass_true = df['AssemblyName'] == 1846771
 is_selected_ass_true = df['AssemblyName'] == int(AssemblyName)
 df_sort_ass = df[is_selected_ass_true]
-print(df_sort_ass.head(10))
+
+print("=========")
+
+is_selected_status = df_sort_ass['Status'] == 'F'
+df_sort_status = df_sort_ass[is_selected_status]
+
+errorCode = parser.get('settings', 'errorCode')
+print(errorCode)
+
+is_selected_errorCode = df_sort_status['StepName'] == str(errorCode)
+df_sort_errorCode = df_sort_status[is_selected_errorCode]
+
+dateList = df_sort_ass['StopTime'].tolist()
+print(dateList)
 
 
-#print(df_filtered_assemblyname.head(20))
+print(f'first date is {dateList[0]}')
+print(f'last date is {dateList[-1]}')
 
-#
+startDate, startTime = dateList[0].split(" ")
+startDate_year, startDate_day, startDate_month  = startDate.split("-")
+start = date(int(startDate_year), int(startDate_month), int(startDate_day))
+print(start)
+
+stopDate, stopTime = dateList[-1].split(" ")
+stopDate_year, stopDate_day, stopDate_month = stopDate.split("-")
+stop = date(int(stopDate_year), int(stopDate_month), int(stopDate_day))
+print(stop)
+
+dateDiff = start - stop
+print(dateDiff)
+#TODO: trzeba jakos podzielic to na przedzialy o danej szerokosci i zliczac liczbe elementow w danych przedzialach
+
 # bins = [2.0,4.0,6.0,8.0,10.0,12.0]
 
 # plt.hist(float_list, bins, label='Diff %', histtype='bar', rwidth=0.8)
