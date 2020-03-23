5c49efd9b73e932948719b71c4a5ea8756d2caac
Dawid Oberda <=>
2020-03-23 15:25:31 +0100

    praca nad przyciskiem load

--- webMain.py
+++ webMain.py
@@ -12,6 +12,7 @@ ALLOWED_EXTENSIONS = set(['xlsx', 'csv'])
 app = Flask(__name__) #tworzymy instancje klasy Flask
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+hist = Histogram()
 
 @app.route("/")
 
@@ -21,19 +22,34 @@ def home():
 
 @app.route("/histogram", methods= ["GET", "POST"])
 def histogram():
-    hist = Histogram()
     if request.method == "POST":
         # if 'reportFile' not in request.files:
         #     flash('No file part')
         report_from_request = request.files["reportFile"]
         filename = secure_filename(report_from_request.filename)
-        #TODO: usunac najpierw wszystko co jest w tym folderze i dopiero pozniej zapisac
+        path_to_del = app.config['UPLOAD_FOLDER']
+        file_list = []
+        for file in os.listdir(path_to_del):
+            file_list.append(file)
+        print(file_list)
+        for file in file_list:
+            os.remove(os.path.join(path_to_del, file))
         report_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         report_from_request.save(report_path)
-        return redirect(url_for('histogram'))
+        report_exist = hist.read_csv(report_path)
+        print(report_exist)
+        #return redirect(url_for('histogram'))
+        return render_template('histogram.html')
     else:
         return render_template('histogram.html')
 #TODO: histogram przerobiony na klase. teraz trzeba zrobic aby ta klase obslugiwalo sie z web
+#TODO: przerobic wszystkie printy na log
+
+#TODO: to dziwnie dziala jak sie wywola najpierw przycisk a pozniej sie chce wczytac plik to sie wysypuje. chyba nie moze tak byc. mnoze sprobowac zrobic tak aby przycisk byl linkiem do miejsca w tym html i tam dac kod?
+@app.route('/dataLoad')
+def SomeFunction():
+    print('In SomeFunction')
+    return render_template('histogram.html')
 
 @app.route("/about")
 def about():
